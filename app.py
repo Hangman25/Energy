709072e30d8513b4732cbbe37374ffbaf638aa1b
atwalsh67 <aaron32walsh@gmail.com>
2025-03-18 21:20:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -67,11 +67,3 @@ def run_continuously(interval=1):
 
 # Stop the background thread
 # stop_run_continuously.set()
-
-if "counter" not in st.session_state:
-    st.session_state.counter = 0
-
-st.session_state.counter += 1
-
-st.header(f"This page has run {st.session_state.counter} times.")
-st.button("Run it again")
